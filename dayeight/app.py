10d50cdc6a22c4858470258648661af00c1343fd
Tech Dream <aspiredigitals@gmail.com>
2022-07-14 17:59:02 -0700

    added elif statements

--- dayeight/app.py
+++ dayeight/app.py
@@ -1,3 +1,14 @@
 number = 5
 if number > 4:
     print("positive")
+elif number < 3:
+    print("no way")
+else:
+    print("negative")
+
+
+name = "isaac"
+if "I" in name:
+    print("yes")
+elif "s" in name:
+    print("yes to all")
